ab858e11bf4c83fc5acc6f51aed51ed0247ea205
Daniel Adeboye <adeboyed93@gmail.com>
2023-10-22 11:24:27 +0100

    Update settings.py

--- buildio/settings.py
+++ buildio/settings.py
@@ -9,8 +9,6 @@ https://docs.djangoproject.com/en/4.1/topics/settings/
 For the full list of settings and their values, see
 https://docs.djangoproject.com/en/4.1/ref/settings/
 """
-SECRET_KEY = 'django-insecure-h+s!u5e!z@h2^cy1-u#*b)t@s8t(i@-@khtr5t%0*-t=(5t5^%'
-
 from pathlib import Path
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
@@ -21,7 +19,6 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-h+s!u5e!z@h2^cy1-u#*b)t@s8t(i@-@khtr5t%0*-t=(5t5^%'
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
